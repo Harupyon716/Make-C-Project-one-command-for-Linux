c846734179cbe340324259a36cec4da472675843
Harupyon716 <rrubypyon@gmail.com>
2024-10-02 22:44:54 +0900

    Update build.sh
    sudo権限無いとディレクトリ作れなかった

--- build/build.sh
+++ build/build.sh
@@ -4,7 +4,7 @@ if [ "$gcc_dir" = "" ];then
     echo "gccが見当たらないためインストールします"
     sudo apt install gcc
 fi    
-mkdir /usr/mbin
+sudo mkdir /usr/mbin
 sudo gcc -o /usr/mbin/mkprjc ../source/main.c
 export PATH=$PATH:/usr/mbin
 echo "構築が完了しました。bashrcに/usr/mbinのパスを追加してください。"
